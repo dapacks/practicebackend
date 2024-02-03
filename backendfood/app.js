2196b6ef06cbc89a3172f86ba6dae283a31d5161
Deepak <60600822+dapacks@users.noreply.github.com>
2024-02-03 15:16:26 +0530

    Update app.js

--- backendfood/app.js
+++ backendfood/app.js
@@ -3,17 +3,17 @@ var express = require('express');
 
 var app = express();
 var cors = require('cors'); // Import the 'cors' module
-app.use(cors({
-  origin: 'http://localhost:5173', // Replace with your frontend domain
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  credentials: true, // Enable credentials (e.g., cookies)
-}));
+
+
 // Use the 'cors' middleware
 app.use(cors());
-app.use((req, res, next) => {
-  res.header({"Access-Control-Allow-Origin": "*"});
-  next();
-})
+  app.use(function(req,res,next){
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, PATCH");
+    res.header("Access-Control-Allow-Headers", "Accept, Content-Type, Authorization, X-Requested-With");
+
+    next();
+  })
 var createError = require('http-errors');
 
 var path = require('path');
